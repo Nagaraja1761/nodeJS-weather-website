1607d01bc422e401331e4e728224586dc3aec39e
Nagaraja S <nagaraja1761@gmail.com>
2021-12-29 09:50:31 +0530

    Add humidity info to website

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,17 +2,16 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=d1392dadf8e2067a16e8c403654240d3&query=' + latitude + ',' + longitude
-    
+
     request({ url, json: true }, (error, response) => {
         if (error) {
             callback('Can\'t connect to the weather sevice!', undefined)
         } else if (response.body.error) {
             callback('Unable to find the location.', undefined)
         } else {
-            callback(undefined, 'The temperature is ' + response.body.current.temperature + ' but feel like ' + response.body.current.feelslike)
+            callback(undefined, 'The temperature is ' + response.body.current.temperature + ' but feel like ' + response.body.current.feelslike + ' and have ' + response.body.current.humidity + '% humidity')
         }
     })
 }
 
 module.exports = forecast;
-
